4020752afb35eed3b3e1165ec4395508b04d5ceb
zakariyyabrewster <149545042+zakariyyabrewster@users.noreply.github.com>
2025-05-16 10:54:36 -0400

    Update unet_model.py

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -3,22 +3,23 @@
 from .unet_parts import *
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, bilinear=True):
+    def __init__(self, n_channels, n_classes, bilinear=True):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.bilinear = bilinear
+        self.n_classes = n_classes
 
         self.inc = DoubleConv(n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
+        self.down4 = Down(512, 1024)
+        self.up1 = Up(1024, 512, bilinear)
+        self.up2 = Up(512, 256, bilinear)
+        self.up3 = Up(256, 128, bilinear)
         self.up4 = Up(128, 64, bilinear)
-        self.out_conv = nn.Conv2d(64, 1, kernel_size=1)
+        self.out_conv = OutConv(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
